978593c28847682dd6247ea9c25bb9ab04367c25
Dorbii <stevemdoris@gmail.com>
2024-12-01 11:48:22 -0600

    init

---
+++ aoc2024/day1.go
@@ -0,0 +1,10 @@
+package aoc2024
+
+import (
+	"fmt"
+)
+
+func Day1() {
+
+	fmt.Println("Day 1")
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"aoc/aoc2024"
+)
+
+func main() {
+	aoc2024.Day1()
+}
